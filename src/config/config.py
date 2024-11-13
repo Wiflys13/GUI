6374415f0d8e04304040b462dabf1df741bbaf25
Wiflys13 <guille112_3@hotmail.com>
2024-11-13 10:34:20 +0100

    URL

--- src/config/config.py
+++ src/config/config.py
@@ -1,10 +1,11 @@
 #config.py
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
 
-# Cargar variables de entorno desde el archivo .env
-load_dotenv()
+# # Cargar variables de entorno desde el archivo .env
+# load_dotenv()
 
-# Leer la IP de la API desde las variables de entorno
-API_IP = os.getenv("API_IP")
-URL_VPN = f'http://{API_IP}'
+# # Leer la IP de la API desde las variables de entorno
+# API_IP = os.getenv("API_IP")
+# URL_VPN = f'http://{API_IP}'
+URL_VPN = 'http://192.168.5.36:8000'
